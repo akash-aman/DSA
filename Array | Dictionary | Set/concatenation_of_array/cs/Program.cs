100349ea4597f4e13bb4e600be92277f6c7b30d3
Akash Aman <sir.akashaman@gmail.com>
2024-07-15 22:11:51 +0530

    feat: Add concatenation of array solution.

---
+++ Array | Dictionary | Set/concatenation_of_array/cs/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program {
+    public static void Main( string[] args ) {
+
+    }
+}
+
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        return nums.Concat(nums).ToArray();
+    }
+}
